6eb929df6d0ed07f806f4e59cac6b677adc96562
Paulo Silva <paulofernando78@gmail.com>
2025-09-25 00:51:25 -0300

    wip

--- src/assets/images/svg-imports.js
+++ src/assets/images/svg-imports.js
@@ -1,3 +1,5 @@
 const gray = "gray"
 
-export const play = `<svg xmlns="http://www.w3.org/2000/svg" width="50px" height="50px" viewBox="0 -960 960 960"  fill="${gray}"><path d="M320-200v-560l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z"/></svg>`
+export const play = `<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 -960 960 960"  fill="${gray}"><path d="M320-200v-560l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z"/></svg>`
+
+export const playDisabled = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="${gray}"><path d="m658-416-56-58-38-36-244-244v-6l440 280-102 64ZM790-56 520-328 320-200v-328L56-792l56-56 736 736-58 56ZM400-448Zm0 102 62-40-62-62v102Zm164-164Z"/></svg>`
--- src/main.js
+++ src/main.js
@@ -1,15 +1,22 @@
-import { play } from "./assets/images/svg-imports";
+import { play, playDisabled } from "./assets/images/svg-imports";
 import { VFX } from "@vfx-js/core";
 
-document.getElementById("play-audio").innerHTML = play;
+document.getElementById("play-audio").innerHTML = playDisabled;
 
 const audio = new Audio("/assets/audio/audio.mp3");
+audio.loop = true;
 
 const playButton = document.querySelector("#play-audio");
 playButton.addEventListener("click", () => {
-  audio.play()
-  audio.loop = true
-})
+  if (audio.paused) {
+    audio.play().then(() => {
+      playButton.innerHTML = play;
+    });
+  } else {
+    audio.pause();
+    playButton.innerHTML = playDisabled;
+  }
+});
 
 const title = document.querySelector("#title");
 const vfx = new VFX();
